0fb58ee9c9c301f579406ec14b4704d6a2196d47
Davi Honorato <davihonoratoctt@gmail.com>
2023-03-07 21:29:04 -0300

    Automação com autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass() {
     return gulp
     .src('assets/stylesheets/sass/**.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass())
+    .pipe(autoprefixer({
+        overrideBrowserslist: [
+            'last 2 versions'
+        ],
+        cascade: false
+    }))
     .pipe(gulp.dest('assets/stylesheets/css/'))
 }
 
