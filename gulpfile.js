8c82fb3a31d865049f771bcc11ba2fa1f99bc2b9
Davi Honorato <davihonoratoctt@gmail.com>
2023-03-08 11:01:26 -0300

    Criando servidor estático com o plugin browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+const { parallel } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
+// Função para compilar o Sass
 function compilaSass() {
     return gulp
     .src('assets/stylesheets/sass/**.scss')
@@ -13,11 +16,28 @@ function compilaSass() {
         cascade: false
     }))
     .pipe(gulp.dest('assets/stylesheets/css/'))
+    .pipe(browserSync.stream())
 }
 
+gulp.task('sass', compilaSass);
+
+// Função do browserSync - Server estático
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+}
+
+gulp.task('browser-sync', browser);
+
+// Função para ficar verificando alterações nos arquivos Sass
 function watch() {
     gulp.watch('assets/stylesheets/sass/**.scss', compilaSass);
 }
 
-gulp.task('sass', compilaSass);
-gulp.task('default', watch);
+gulp.task('watch', watch);
+
+// Função default
+gulp.task('default', parallel('watch', 'browser-sync'))
