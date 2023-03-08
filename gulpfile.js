240c363431c7320374d7a695096eedce8165d918
Davi Honorato <davihonoratoctt@gmail.com>
2023-03-08 11:23:08 -0300

    Sincronizando o browser a cada alteração feita no arquivo html + comentários

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
+// Adiciona os módulos intalados
 const { parallel } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
-// Função para compilar o Sass
+// Função e task para compilar o Sass já com os prefixos
 function compilaSass() {
     return gulp
     .src('assets/stylesheets/sass/**.scss')
@@ -21,7 +22,7 @@ function compilaSass() {
 
 gulp.task('sass', compilaSass);
 
-// Função do browserSync - Server estático
+// Função e task do browserSync - Server estático
 function browser() {
     browserSync.init({
         server: {
@@ -32,12 +33,13 @@ function browser() {
 
 gulp.task('browser-sync', browser);
 
-// Função para ficar verificando alterações nos arquivos Sass
+// Função e task para verificar alterações nos arquivos .sass e .html
 function watch() {
     gulp.watch('assets/stylesheets/sass/**.scss', compilaSass);
+    gulp.watch('*.html').on('change', browserSync.reload);
 }
 
 gulp.task('watch', watch);
 
-// Função default
+// Task padrão do gulp para executar, paralelamente, as tasks 'watch' e 'browser-sync'
 gulp.task('default', parallel('watch', 'browser-sync'))
